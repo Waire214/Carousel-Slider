3854c8eda02e758575250a21a20b2ebd77c5abbc
Waire <waire.tega@gmail.com>
2021-02-28 23:25:00 +0100

    name change

--- design.js
+++ design.js
@@ -5,14 +5,21 @@ const indicatorParent = document.querySelector('.controls ul');
 const indicators = document.querySelectorAll('.controls li');
 const list = document.querySelector(".buttons");
 let index = 0;
-let timer = setInterval(autoslide,1000);
 
+//set autoslide to the nextSlide function at the interval of 1000ms
+let timer = setInterval(nextSlide,1000);
+
+//on clicking the previous key, call the prevSlide function
 prev.addEventListener("click", function(){
   prevSlide();
 })
+
+//on clicking the next key, call the nextSlide function
 next.addEventListener("click", function(){
   nextSlide();
 })
+
+//on clicking the buttons, go to the next slide
 function indicateFunc(){
   indicators.forEach((indicator, i) => {
     indicator.addEventListener('click', () => {
@@ -25,6 +32,7 @@ function indicateFunc(){
 });
 }
 indicateFunc();
+
 function prevSlide(){
   if(index === 0){
     index = 3;
@@ -33,6 +41,7 @@ function prevSlide(){
   }
   changeSlide();
 }
+
 function nextSlide(){
   if(index === 3){
     index = 0;
@@ -41,38 +50,30 @@ function nextSlide(){
   }
   changeSlide();
 }
+
 function changeSlide(){
   document.querySelector('.controls .selected').classList.remove('selected');
   indicatorParent.children[index].classList.add('selected');
   slider.style.transform = 'translateX(' + (index) * -25 + '%)';
 }
+//remove autoslide when you move mouse over the prev/next/button keys
+//add autoslide when you move mouse out
 prev.addEventListener("mouseover", function(){
   resetTimer();
 })
 prev.addEventListener("mouseout", function(){
-  timer=setInterval(autoslide,1000);
-})
+  timer = setInterval(nextSlide,1000);})
 next.addEventListener("mouseover", function(){
   resetTimer();
 })
 next.addEventListener("mouseout", function(){
-  timer=setInterval(autoslide,1000);
-})
+  timer = setInterval(nextSlide,1000);})
 list.addEventListener("mouseover", function(){
   resetTimer();
 })
 list.addEventListener("mouseout", function(){
-  timer=setInterval(autoslide,1000);
-})
-
+  timer = setInterval(nextSlide,1000);})
+//function to clear autoslide
 function resetTimer(){
-  // when click to indicator or controls button 
-  // stop timer 
   clearInterval(timer);
-  // then started again timer
-  //timer=setInterval(autoslide,1500);
-}
-
-function autoslide(){
-  nextSlide();
 }
